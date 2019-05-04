e0aa3eb5f6678dffb265ba4ca4a94744db952370
Matteo Cafasso <noxdafox@gmail.com>
2019-05-04 15:59:55 +0300

    Add application module
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

---
+++ lib/application.ex
@@ -0,0 +1,9 @@
+defmodule RabbitMQ.CloudWatchExporter.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = [RabbitMQ.CloudWatchExporter.Exporter]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
