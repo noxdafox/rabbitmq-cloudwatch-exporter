2f717fd5e2a5459167e057eeba19526139bf666a
Matteo Cafasso <noxdafox@gmail.com>
2019-05-04 16:00:12 +0300

    Remove old module
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- lib/rabbitmq_metrics_collector.ex
+++
@@ -1,9 +0,0 @@
-defmodule RabbitMQ.CloudWatchExporter.Application do
-  use Application
-
-  def start(_type, _args) do
-    children = [RabbitMQ.CloudWatchExporter.Collector]
-
-    Supervisor.start_link(children, strategy: :one_for_one)
-  end
-end
