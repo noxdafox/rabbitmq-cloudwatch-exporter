481f88e0f3bb658ca28e719b791af4dae4689b38
Matteo Cafasso <noxdafox@gmail.com>
2020-04-26 14:41:47 +0300

    issue #16: support metrics whitelist filter in exporter
    Metrics configuration refactoring.
    
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- lib/rabbitmq_cloudwatch_exporter/exporter.ex
+++ lib/rabbitmq_cloudwatch_exporter/exporter.ex
@@ -53,7 +53,7 @@ defmodule RabbitMQCloudWatchExporter.Exporter do
     cluster = RabbitNodes.cluster_name()
 
     options[:collectors]
-      |> Enum.flat_map(fn(c) -> @collectors[c].(options[:regex_patterns]) end)
+      |> Enum.flat_map(fn({c, o}) -> @collectors[c].(o) end)
       |> Enum.filter(fn(m) -> m[:value] != nil end)
       |> Enum.map(fn(m) ->
                     Keyword.update(m, :dimensions, [], &(([{"Cluster", cluster} | &1])))
@@ -69,13 +69,18 @@ defmodule RabbitMQCloudWatchExporter.Exporter do
 
   # Retrieve application options
   defp application_options() do
-    [period: Application.get_env(@appname, :export_period, @default_export_period),
-     collectors: Application.get_env(@appname, :metrics, []),
+    metrics = Application.get_env(@appname, :metrics, [])
+    collectors_options = @collectors
+      |> Map.keys()
+      |> Enum.flat_map(fn(c) -> metrics_options(metrics, c) end)
+      |> Enum.map(&compile_regex_patterns/1)
+
+    [aws: aws_options(),
+     period: Application.get_env(@appname, :export_period, @default_export_period),
      namespace: Application.get_env(@appname, :namespace, @default_namespace)
        |> to_string()
        |> String.trim("\""),
-     aws: aws_options(),
-     regex_patterns: compile_regex_patterns()]
+     collectors: collectors_options]
   end
 
   # Retrieve and sanitize AWS specific options
@@ -87,15 +92,42 @@ defmodule RabbitMQCloudWatchExporter.Exporter do
                   end)
   end
 
+  # Retrieve options for enabled metrics
+  defp metrics_options(metrics, name) do
+    if metrics |> Keyword.get(name, []) |> Keyword.get(:enable, false) do
+      options = metrics
+        |> Keyword.get(name)
+        |> Keyword.take([:export_regex, :export_metrics])
+        |> Enum.map(fn({k, o}) -> {k, sanitize_option(o)} end)
+
+      [{name, options}]
+    else
+      []
+    end
+  end
+
+  # Sanitize all values of the given option
+  defp sanitize_option(options = [option | _]) when is_list(option) do
+    Enum.map(options, &sanitize_option/1)
+  end
+
+  defp sanitize_option(option) when is_atom(option) do
+    sanitize_option(option) |> String.to_atom()
+  end
+
+  defp sanitize_option(option) do
+    option |> to_string() |> String.trim("\"")
+  end
+
   # Compile regular expressions provided in configuration
-  defp compile_regex_patterns() do
-    Application.get_env(@appname, :export_regex, [])
-      |> Keyword.take([:exchange, :queue, :connection, :channel])
-      |> Enum.map(fn({option, value}) ->
-                    pattern = value |> to_string() |> String.trim("\"")
-                    {option, Regex.compile!(pattern)}
-                  end)
-      |> Keyword.new()
+  def compile_regex_patterns({metric, options}) do
+    regex_metrics = [:exchange, :queue, :connection, :channel]
+
+    if metric in regex_metrics and Keyword.has_key?(options, :export_regex) do
+      {metric, Keyword.update!(options, :export_regex, &Regex.compile!/1)}
+    else
+      {metric, options}
+    end
   end
 
   # Export the collected metrics splitting the requests in chunks according to AWS CW limits
