58ef0cc460f1eee0762fde430ebb65c01c6530e3
Matteo Cafasso <noxdafox@gmail.com>
2022-01-16 21:59:01 +0200

    mix: release 1.0.3
    Signed-off-by: Matteo Cafasso <noxdafox@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RabbitMQCloudWatchExporter.Mixfile do
   def project do
     [
       app: :rabbitmq_cloudwatch_exporter,
-      version: "1.0.2",
+      version: "1.0.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps_path: "deps",
