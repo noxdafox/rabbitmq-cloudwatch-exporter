9d69cd8063515d41c37f175e18ac862a30ddd7bd
Matteo Cafasso <noxdafox@gmail.com>
2025-04-07 00:08:08 +0300

    mix: build archives in `DIST_DIR`

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule RabbitMQCloudWatchExporter.Mixfile do
   use Mix.Project
 
-  def project do
+  def project() do
     [
       app: :rabbitmq_cloudwatch_exporter,
       version: "1.0.6",
@@ -20,18 +20,18 @@ defmodule RabbitMQCloudWatchExporter.Mixfile do
     ]
   end
 
-  defp deps(deps_dir) do
+  defp deps(_deps_dir) do
     [
       {:ex_aws, "~> 2.5.8"},
       {:ex_aws_cloudwatch, "~> 2.0.4"},
       {:singleton, "~> 1.4.0"},
       {:jason, "~> 1.4.4"},
       {:hackney, "~> 1.23.0"},
-      {:mix_task_archive_deps, github: "noxdafox/mix_task_archive_deps"}
+      {:mix_task_archive_deps, github: "rabbitmq/mix_task_archive_deps"}
     ]
   end
 
-  defp aliases do
+  defp aliases() do
     [
       make_deps: [
         "deps.get",
@@ -43,10 +43,14 @@ defmodule RabbitMQCloudWatchExporter.Mixfile do
         "compile"
       ],
       make_archives: [
-        "archive.build.deps",
-        "archive.build.elixir",
-        "archive.build.all"
+        "archive.build.deps --destination=#{dist_dir()}",
+        "archive.build.elixir --destination=#{dist_dir()}",
+        "archive.build.all --destination=#{dist_dir()}"
       ]
     ]
+  end
+
+  defp dist_dir() do
+    System.get_env("DIST_DIR", "plugins")
   end
 end
